78db2433b489250dbf0302bde9b23d54ed2fe3b1
Tiffany Moeller <tmoellerart@gmail.com>
2018-03-14 10:57:47 -0500

    test execution

--- scrape_providers.py
+++ scrape_providers.py
@@ -4,5 +4,5 @@ import data_scrape_functions as dsf
 driver = dsf.instantiate_driver()
 dsf.navigate_to_site(driver)
 driver.find_element_by_partial_link_text("Chicago").send_keys(Keys.RETURN)
-providers_data = dsf.pagenate_all_providers(driver, max_pages=10)
+providers_data = dsf.pagenate_all_providers(driver, max_pages=1)
 
