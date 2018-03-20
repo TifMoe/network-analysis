1535dd041db61c4073317f76c758fcd6ac49c48e
Tiffany Moeller <tmoellerart@gmail.com>
2018-03-20 16:51:55 -0500

    push parameters to top and allow restart on specific page

--- scrape_providers.py
+++ scrape_providers.py
@@ -1,8 +1,25 @@
 from selenium.webdriver.common.keys import Keys
 import data_scrape_functions as dsf
+import time
 
-driver = dsf.instantiate_driver()
+# Set Parameters
+city_name = 'Chicago'
+start_page = 31
+max_pages = 2
+save_interval = 1
+
+driver = dsf.instantiate_driver(browser=True)
 dsf.navigate_to_site(driver)
-driver.find_element_by_partial_link_text("Chicago").send_keys(Keys.RETURN)
-providers_data = dsf.pagenate_all_providers(driver, max_pages=1)
+time.sleep(3)
+
+driver.find_element_by_partial_link_text(city_name).send_keys(Keys.RETURN)
+
+if start_page > 1:
+    url = driver.current_url
+    new_url = url + f'-{start_page}'
+    driver.get(new_url)
+
+providers_data = dsf.pagenate_all_providers(driver,
+                                            max_pages=max_pages,
+                                            save_interval_pages=save_interval)
 
