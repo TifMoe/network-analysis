de30bc0e1dcf07dd0f1dd9699bf8cf4fc615342d
Tiffany Moeller <tmoellerart@gmail.com>
2018-03-31 21:13:44 -0500

    add login page

--- app.py
+++ app.py
@@ -1,23 +1,39 @@
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 from src.features.fetch_data import fetch_data
+import os
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
             error = 'Invalid Credentials. Please try again.'
         else:
-            return redirect(url_for('index'))
+            return redirect(url_for('admin'))
     return render_template('landing.html', error=error)
+
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+
+
+@app.route("/admin")
+def admin():
+    return render_template("index.html")
 
 
 @app.route("/data")
